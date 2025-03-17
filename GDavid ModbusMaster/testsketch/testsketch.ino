b00cca7a30355ac0a0fdb90e2536b077e9487293
GDavid <99947019+GaiolaDavid@users.noreply.github.com>
2025-03-18 01:34:34 +0200

    Update testsketch.ino

--- GDavid ModbusMaster/testsketch/testsketch.ino
+++ GDavid ModbusMaster/testsketch/testsketch.ino
@@ -1,19 +1,29 @@
 #include <SoftwareSerial.h>
 #define rxPin 11
 #define txPin 10
+#define enPin 13
 #define inverse_logic false
+int counter = 0;
+SoftwareSerial serial485 = SoftwareSerial(rxPin, txPin, inverse_logic);
 
 void setup() {
+  pinMode(enPin, OUTPUT);
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
-  SoftwareSerial serial485(rxPin, txPin, inverse_logic);
-  serial485.begin(115200);
+  digitalWrite(enPin,HIGH);
+  serial485.begin(9600);
+  Serial.begin(57600);
+  //serial485.write("Test");
+  Serial.println("Test");
+  serial485.write(":RUbEnaBA\n\r");
   
-
 
 }
 
 void loop() {
+  
+  
+  
   
 
 }
