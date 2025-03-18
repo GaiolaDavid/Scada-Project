04c164bc8af53d55803f49684ea13cfa6274ecef
MagyarosiRuben <magyarosi.richard@student.ms.sapientia.ro>
2025-03-18 16:19:30 +0200

    Update in communication

--- MRuben Measurement 1/sketch_mar11a/sketch_mar11a.ino
+++ MRuben Measurement 1/sketch_mar11a/sketch_mar11a.ino
@@ -14,39 +14,40 @@ void setup() {
   pinMode(enPin, OUTPUT);
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
-  digitalWrite(enPin,LOW);
+  digitalWrite(enPin,HIGH);
   serial485.begin(9600);
   Serial.begin(57600);
   Serial.println("START");
+  serial485.print(":TEST\n\r");
 }
 
 void loop() {
-  if(serial485.available()) {
-    int curreadi=serial485.read();
-    char curread=(char)curreadi;
-    if(flag) {
-      strncat(string,&curread,1);
-      if(curreadi==10) {
-        flag1=true;
-      }
-      if(flag1 && curreadi == 13) {
-        flag1=false;
-        flag=false;
-        flag2=true;
-        Serial.println("String got");
-      }
-    }
-    if(curreadi==58) {
-      flag=true;
-      strcpy(string,"");
-      Serial.println("String read start");
-      Serial.println(string);
-    }
-  }
-  if(flag2) {
-    Serial.println("String is");
-    Serial.print(string);
-    flag2=false;
-  }
+  // if(serial485.available()) {
+  //   int curreadi=serial485.read();
+  //   char curread=(char)curreadi;
+  //   if(flag) {
+  //     strncat(string,&curread,1);
+  //     if(curreadi==10) {
+  //       flag1=true;
+  //     }
+  //     if(flag1 && curreadi == 13) {
+  //       flag1=false;
+  //       flag=false;
+  //       flag2=true;
+  //       Serial.println("String got");
+  //     }
+  //   }
+  //   if(curreadi==58) {
+  //     flag=true;
+  //     strcpy(string,"");
+  //     Serial.println("String read start");
+  //     Serial.println(string);
+  //   }
+  // }
+  // if(flag2) {
+  //   Serial.println("String is");
+  //   Serial.print(string);
+  //   flag2=false;
+  // }
 
 }
