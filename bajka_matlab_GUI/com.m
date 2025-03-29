59bd55e8916222dfee7e0b12ef68360217f4fab1
Bajka Adorjan <bajka.adorjan@student.ms.sapientia.ro>
2025-03-29 14:12:49 +0200

    Vegleges verzio

--- bajka_matlab_GUI/com.m
+++ bajka_matlab_GUI/com.m
@@ -1,6 +1,8 @@
-device = serialport("COM8",9600);
-datatype = "string";
+device = serialport("COM7", 9600); % Open serial port
+configureTerminator(device, "LF"); % Set Line Feed (\n) as the terminator
+flush(device); % Clear the buffer before starting
 
 for i = 1:100
-    data = read(device,25,datatype)
+    data = readline(device); % Read full line until \n
+    disp(data); % Display the full string
 end
